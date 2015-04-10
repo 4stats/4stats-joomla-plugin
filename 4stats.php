a97f499dd128ede3e0b7f071d9d1c8a288eb30ce
nipuman <nico.puhlmann@gmail.com>
2015-04-10 13:21:12 +0200

    Bug fixes, code indents

--- 4stats.php
+++ 4stats.php
@@ -20,52 +20,55 @@ jimport( 'joomla.html.html' );
 
 class plgSystem4stats extends JPlugin
 {
-	
-	function onBeforeRender()
-	{
-		// get and integrate tracking code.
-		$trackingCode = $this->trackingCode();
-		$doc = &JFactory::getDocument();
-		$doc->addCustomTag($trackingCode);
-	}
+    
+    function onBeforeRender()
+    {
+        // get and integrate tracking code.
+        $trackingCode = $this->trackingCode();
+        @$doc = &JFactory::getDocument();
+        if(method_exists($doc, "addCustomTag"))
+        {
+            $doc->addCustomTag($trackingCode);
+        }
+    }
 
-	/**
-	 * trackingCode - generate the 4stats tracking code.
-	 * @param null
-	 * @return string
-	 */
-	function trackingCode(){
-		
-		
-		$application = &JFactory::getApplication();
-		
-		// params
-		$enabled = $this->params->get('enabled', 0);
-		$project_id = $this->params->get('project_id', '');
-		$ignoreadmins = $this->params->get('ignoreadmins', 0);
-		$trackuser = $this->params->get('trackuser', 0);
+    /**
+     * trackingCode - generate the 4stats tracking code.
+     * @param null
+     * @return string
+     */
+    function trackingCode(){
+        
+        
+        @$application = &JFactory::getApplication();
+        
+        // params
+        $enabled = $this->params->get('enabled', 0);
+        $project_id = $this->params->get('project_id', '');
+        $ignoreadmins = $this->params->get('ignoreadmins', 0);
+        $trackuser = $this->params->get('trackuser', 0);
 
         
         if($enabled != 1 || !$project_id || ($ignoreadmins == 1 && $application->isAdmin()) || JRequest::getVar('format','html') != 'html')
-		{
-			return '';
-		}
-		
-		// get application information
-		$application =& JFactory::getApplication('site');
-		$application->initialise();
-		$user = &JFactory::getUser();
-		
-		$trackingCode = "<script>\r\n";
-		$trackingCode .= "var _fss=_fss||{}; _fss.siteId = {$project_id};\r\n";
-		if($trackuser == 1 && $user->guest == 0)
-		{
-			$trackingCode .= "_fss.identify = {username:'" . $user->username . "', name:'" . $user->name . "', email:'" . $user->email . "', registerdate:'" . $user->registerDate . "'};\r\n";
-		}
-		$trackingCode .= '(function(){var e="fourstats",a=window,c=["track","identify","config","register"],b=function(){var d=0,f=this;for(f._fs=[],d=0;c.length>d;d++){(function(j){f[j]=function(){return f._fs.push([j].concat(Array.prototype.slice.call(arguments,0))),f}})(c[d])}};a[e]=a[e]||new b;var i=document;var h=i.createElement("script");h.type="text/javascript";h.async=true;h.src=document.location.protocol+"//4stats.de/track.js";var g=i.getElementsByTagName("script")[0];g.parentNode.insertBefore(h,g)})();' . "\r\n";
-		$trackingCode .= "</script>\r\n";
-		
+        {
+            return '';
+        }
+        
+        // get application information
+        @$application =& JFactory::getApplication('site');
+        $application->initialise();
+        @$user = &JFactory::getUser();
+        
+        $trackingCode = "<script>\r\n";
+        $trackingCode .= "var _fss=_fss||{}; _fss.siteId = {$project_id};\r\n";
+        if($trackuser == 1 && $user->guest == 0)
+        {
+            $trackingCode .= "_fss.identify = {username:'" . $user->username . "', name:'" . $user->name . "', email:'" . $user->email . "', registerdate:'" . $user->registerDate . "'};\r\n";
+        }
+        $trackingCode .= '(function(){var e="stetic",a=window,c=["track","identify","config","set","unset","register","unregister","increment","alias"],b=function(){var d=0,f=this;for(f._fs=[],d=0;c.length>d;d++){(function(j){f[j]=function(){return f._fs.push([j].concat(Array.prototype.slice.call(arguments,0))),f}})(c[d])}};a[e]=a[e]||new b;a.fourstats=a.fourstats||new b;var i=document;var h=i.createElement("script");h.type="text/javascript";h.async=true;h.src="//stetic.com/t.js";var g=i.getElementsByTagName("script")[0];g.parentNode.insertBefore(h,g)})();' . "\r\n";
+        $trackingCode .= "</script>\r\n";
+        
 
-		return $trackingCode;
-	}
+        return $trackingCode;
+    }
 }
